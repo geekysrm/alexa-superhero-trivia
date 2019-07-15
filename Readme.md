5aa1244596cb5b5a86fab7c9842dd8056131b62a
geekysrm <soumyacool2012@gmail.com>
2019-07-15 22:11:55 +0530

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## Alexa Harry Potter Trivia Skill
---
+++ lambda/custom/questions.js
@@ -0,0 +1,719 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_GB: [
+    {
+      "What was the name of Mr. Crouch's house elf?": [
+        "Winky",
+        "Blinky",
+        "Slinky",
+        "Dobby"
+      ]
+    },
+    {
+      "Who is the teacher who Harry thinks is working with Voldemort?": [
+        "Snape",
+        "Quirrell",
+        "Sprout",
+        "McGonagall"
+      ]
+    },
+    {
+      " Who taught History of Magic?": [
+        "Professor Binns",
+        "Professor Sinistra",
+        "Professor Flitwick",
+        "Professor Lupin"
+      ]
+    },
+    {
+      "What happens to Professor Lockhart?": [
+        "He loses his memory",
+        "He dies",
+        "He runs away",
+        "He is fired"
+      ]
+    },
+    {
+      " In which year (1-7) did Harry go on his first date with Cho Chang?": [
+        "5",
+        "4",
+        "6",
+        "3"
+      ]
+    },
+    {
+      "What did Dumbledore leave to Ron in his will? ": [
+        "The Deluminator",
+        "Fawkes the Phoenix",
+        "The golden snitch",
+        "The Tales of Beedle the Bard"
+      ]
+    },
+    {
+      "How old was Voldemort when he opened the Chamber of Secrets?": [
+        "16 years",
+        "17 years",
+        "18 years",
+        "21 years"
+      ]
+    },
+    {
+      "Who was the master of the Elder Wand prior to Harry?": [
+        "Draco Malfoy",
+        "Minerva McGonagall",
+        "Severus Snape",
+        "Lord Voldemort"
+      ]
+    },
+    {
+      "The group The Marauders were made up of Hogwarts students. Which of the options was James Potters nickname and Animagus form?": [
+        "Prongs, Stag",
+        "Potty, Llama",
+        "Hooves, Donkey",
+        "Moosey, Elk"
+      ]
+    },
+    {
+      "Which of these options was Peter Pettigrews nickname and Animagus form?": [
+        "Wormtail, Rat",
+        "Spaghettiback, Mouse",
+        "Fur-ears, Bat",
+        "Smallsnout, Chinchilla"
+      ]
+    },
+    {
+      "What colour are Harry Potters eyes?": ["Green", "Blue", "Black", "Brown"]
+    },
+    {
+      "What was Remus Lupins nickname and Animagus form?": [
+        "Moony, Werewolf",
+        "Howly, Grey Wolf",
+        "Moony, Grey Wolf",
+        "Howly, Werewolf"
+      ]
+    },
+    {
+      "What is the name of Ron and Hermione's daughter?": [
+        "Rose",
+        "Lily",
+        "Cardi",
+        "Hugo"
+      ]
+    },
+    {
+      "What is the name of Ron and Hermione's son?": [
+        "Hugo",
+        "Ron Junior",
+        "Remus",
+        "Neville"
+      ]
+    },
+    {
+      "Which is the house ghost of Hufflepuff?": [
+        "The Fat Friar",
+        "The Bloody Baron",
+        "The Grey Lady",
+        "Nearly Headless Nick"
+      ]
+    },
+    {
+      "Which champion is the only one to die in the 1994-1995 Triwizard Tournament?": [
+        "Cedric Diggory",
+        "Harry Potter",
+        "Fleur Delacour",
+        "Viktor Krum"
+      ]
+    },
+    {
+      "Who was NOT a member of the 'Slug Club'?": [
+        "Ron",
+        "Harry",
+        "Hermione",
+        "Melinda"
+      ]
+    },
+    {
+      "Which type of magical creature carries Sirius Black away from Hogwarts?": [
+        "Hippogriff",
+        "Thestral",
+        "Unicorn",
+        "Dragon"
+      ]
+    },
+    {
+      "Which is NOT a shop in Diagon Alley?": [
+        "Honeydukes",
+        "Madam Malkin's Robes for All Occasions",
+        "Flourish & Blott's",
+        "Eeylops Owl Emporium"
+      ]
+    },
+    {
+      "Which of the following is NOT a protection for the Philosopher's (Sorcerer's) Stone?": [
+        "Cornish Pixies",
+        "Wizard's Chess ",
+        "Devil's Snare",
+        "Mirror of Erised"
+      ]
+    },
+    {
+      "Which of the following was NOT a Death Eater?": [
+        "Bathilda Bagshot",
+        "Lucius Malfoy",
+        "Draco Malfoy",
+        "Bellatrix Lestrange"
+      ]
+    },
+    {
+      "How many broomsticks are flown in a full game of Quidditch?": [
+        "15",
+        "16",
+        "18",
+        "25"
+      ]
+    },
+    {
+      "Which Polyjuice Potion ingredients must be acquired at the full moon?": [
+        "Fluxweed",
+        "Knotgrass",
+        "A bit of whoever you wish to turn into",
+        "Newt spleen"
+      ]
+    },
+    {
+      "Before using the Firebolt gifted to him in The Prisoner of Azkaban, Harry Potter flew on which of these broomstick models?": [
+        "Nimbus 2000",
+        "Cleansweep Eleven",
+        "Moontrimmer",
+        "Thunderbolt VII"
+      ]
+    },
+    {
+      "The Goblins of Gringotts Wizarding Bank navigate the vaults of their establishment using which of these modes of transportation?": [
+        "Mine cart",
+        "Elevator",
+        "Magic carpet",
+        "Monorail"
+      ]
+    },
+    {
+      "What person from the Wizarding World does Harry discover living just down the road?": [
+        "Mrs. Figg",
+        "Tonks",
+        "Filch",
+        "Susan Bones"
+      ]
+    },
+    {
+      "Floo Powder, as a substance, allows users to travel by which means (as seen in The Chamber of Secrets)?": [
+        "Through fireplaces",
+        "Inside animals",
+        "n the wind",
+        "Through the sewers"
+      ]
+    },
+    {
+      "Who or what attacks Ron in the Ministry of Magic?": [
+        "a brain",
+        "a Time-turner",
+        "Bellatrix",
+        "Dolohov"
+      ]
+    },
+    {
+      "The Knight Bus, used by Harry in The Prisoner of Azkaban, contains how many levels?": [
+        "3",
+        "2",
+        "5",
+        "1"
+      ]
+    },
+    {
+      "Half eagle and half horse, what creature is ridden by Harry and Hermione in The Prisoner of Azkaban after saving its life?": [
+        "Hippogriff",
+        "Centaur",
+        "Dragon",
+        "Griffin"
+      ]
+    }
+  ],
+  QUESTIONS_EN_US: [
+    {
+      "What was the name of Mr. Crouch's house elf?": [
+        "Winky",
+        "Blinky",
+        "Slinky",
+        "Dobby"
+      ]
+    },
+    {
+      "Who is the teacher who Harry thinks is working with Voldemort?": [
+        "Snape",
+        "Quirrell",
+        "Sprout",
+        "McGonagall"
+      ]
+    },
+    {
+      " Who taught History of Magic?": [
+        "Professor Binns",
+        "Professor Sinistra",
+        "Professor Flitwick",
+        "Professor Lupin"
+      ]
+    },
+    {
+      "What happens to Professor Lockhart?": [
+        "He loses his memory",
+        "He dies",
+        "He runs away",
+        "He is fired"
+      ]
+    },
+    {
+      " In which year (1-7) did Harry go on his first date with Cho Chang?": [
+        "5",
+        "4",
+        "6",
+        "3"
+      ]
+    },
+    {
+      "What did Dumbledore leave to Ron in his will? ": [
+        "The Deluminator",
+        "Fawkes the Phoenix",
+        "The golden snitch",
+        "The Tales of Beedle the Bard"
+      ]
+    },
+    {
+      "How old was Voldemort when he opened the Chamber of Secrets?": [
+        "16 years",
+        "17 years",
+        "18 years",
+        "21 years"
+      ]
+    },
+    {
+      "Who was the master of the Elder Wand prior to Harry?": [
+        "Draco Malfoy",
+        "Minerva McGonagall",
+        "Severus Snape",
+        "Lord Voldemort"
+      ]
+    },
+    {
+      "The group The Marauders were made up of Hogwarts students. Which of the options was James Potters nickname and Animagus form?": [
+        "Prongs, Stag",
+        "Potty, Llama",
+        "Hooves, Donkey",
+        "Moosey, Elk"
+      ]
+    },
+    {
+      "Which of these options was Peter Pettigrews nickname and Animagus form?": [
+        "Wormtail, Rat",
+        "Spaghettiback, Mouse",
+        "Fur-ears, Bat",
+        "Smallsnout, Chinchilla"
+      ]
+    },
+    {
+      "What colour are Harry Potters eyes?": ["Green", "Blue", "Black", "Brown"]
+    },
+    {
+      "What was Remus Lupins nickname and Animagus form?": [
+        "Moony, Werewolf",
+        "Howly, Grey Wolf",
+        "Moony, Grey Wolf",
+        "Howly, Werewolf"
+      ]
+    },
+    {
+      "What is the name of Ron and Hermione's daughter?": [
+        "Rose",
+        "Lily",
+        "Cardi",
+        "Hugo"
+      ]
+    },
+    {
+      "What is the name of Ron and Hermione's son?": [
+        "Hugo",
+        "Ron Junior",
+        "Remus",
+        "Neville"
+      ]
+    },
+    {
+      "Which is the house ghost of Hufflepuff?": [
+        "The Fat Friar",
+        "The Bloody Baron",
+        "The Grey Lady",
+        "Nearly Headless Nick"
+      ]
+    },
+    {
+      "Which champion is the only one to die in the 1994-1995 Triwizard Tournament?": [
+        "Cedric Diggory",
+        "Harry Potter",
+        "Fleur Delacour",
+        "Viktor Krum"
+      ]
+    },
+    {
+      "Who was NOT a member of the 'Slug Club'?": [
+        "Ron",
+        "Harry",
+        "Hermione",
+        "Melinda"
+      ]
+    },
+    {
+      "Which type of magical creature carries Sirius Black away from Hogwarts?": [
+        "Hippogriff",
+        "Thestral",
+        "Unicorn",
+        "Dragon"
+      ]
+    },
+    {
+      "Which is NOT a shop in Diagon Alley?": [
+        "Honeydukes",
+        "Madam Malkin's Robes for All Occasions",
+        "Flourish & Blott's",
+        "Eeylops Owl Emporium"
+      ]
+    },
+    {
+      "Which of the following is NOT a protection for the Philosopher's (Sorcerer's) Stone?": [
+        "Cornish Pixies",
+        "Wizard's Chess ",
+        "Devil's Snare",
+        "Mirror of Erised"
+      ]
+    },
+    {
+      "Which of the following was NOT a Death Eater?": [
+        "Bathilda Bagshot",
+        "Lucius Malfoy",
+        "Draco Malfoy",
+        "Bellatrix Lestrange"
+      ]
+    },
+    {
+      "How many broomsticks are flown in a full game of Quidditch?": [
+        "15",
+        "16",
+        "18",
+        "25"
+      ]
+    },
+    {
+      "Which Polyjuice Potion ingredients must be acquired at the full moon?": [
+        "Fluxweed",
+        "Knotgrass",
+        "A bit of whoever you wish to turn into",
+        "Newt spleen"
+      ]
+    },
+    {
+      "Before using the Firebolt gifted to him in The Prisoner of Azkaban, Harry Potter flew on which of these broomstick models?": [
+        "Nimbus 2000",
+        "Cleansweep Eleven",
+        "Moontrimmer",
+        "Thunderbolt VII"
+      ]
+    },
+    {
+      "The Goblins of Gringotts Wizarding Bank navigate the vaults of their establishment using which of these modes of transportation?": [
+        "Mine cart",
+        "Elevator",
+        "Magic carpet",
+        "Monorail"
+      ]
+    },
+    {
+      "What person from the Wizarding World does Harry discover living just down the road?": [
+        "Mrs. Figg",
+        "Tonks",
+        "Filch",
+        "Susan Bones"
+      ]
+    },
+    {
+      "Floo Powder, as a substance, allows users to travel by which means (as seen in The Chamber of Secrets)?": [
+        "Through fireplaces",
+        "Inside animals",
+        "n the wind",
+        "Through the sewers"
+      ]
+    },
+    {
+      "Who or what attacks Ron in the Ministry of Magic?": [
+        "a brain",
+        "a Time-turner",
+        "Bellatrix",
+        "Dolohov"
+      ]
+    },
+    {
+      "The Knight Bus, used by Harry in The Prisoner of Azkaban, contains how many levels?": [
+        "3",
+        "2",
+        "5",
+        "1"
+      ]
+    },
+    {
+      "Half eagle and half horse, what creature is ridden by Harry and Hermione in The Prisoner of Azkaban after saving its life?": [
+        "Hippogriff",
+        "Centaur",
+        "Dragon",
+        "Griffin"
+      ]
+    }
+  ],
+  QUESTIONS_EN_IN: [
+    {
+      "What was the name of Mr. Crouch's house elf?": [
+        "Winky",
+        "Blinky",
+        "Slinky",
+        "Dobby"
+      ]
+    },
+    {
+      "Who is the teacher who Harry thinks is working with Voldemort?": [
+        "Snape",
+        "Quirrell",
+        "Sprout",
+        "McGonagall"
+      ]
+    },
+    {
+      " Who taught History of Magic?": [
+        "Professor Binns",
+        "Professor Sinistra",
+        "Professor Flitwick",
+        "Professor Lupin"
+      ]
+    },
+    {
+      "What happens to Professor Lockhart?": [
+        "He loses his memory",
+        "He dies",
+        "He runs away",
+        "He is fired"
+      ]
+    },
+    {
+      " In which year (1-7) did Harry go on his first date with Cho Chang?": [
+        "5",
+        "4",
+        "6",
+        "3"
+      ]
+    },
+    {
+      "What did Dumbledore leave to Ron in his will? ": [
+        "The Deluminator",
+        "Fawkes the Phoenix",
+        "The golden snitch",
+        "The Tales of Beedle the Bard"
+      ]
+    },
+    {
+      "How old was Voldemort when he opened the Chamber of Secrets?": [
+        "16 years",
+        "17 years",
+        "18 years",
+        "21 years"
+      ]
+    },
+    {
+      "Who was the master of the Elder Wand prior to Harry?": [
+        "Draco Malfoy",
+        "Minerva McGonagall",
+        "Severus Snape",
+        "Lord Voldemort"
+      ]
+    },
+    {
+      "The group The Marauders were made up of Hogwarts students. Which of the options was James Potters nickname and Animagus form?": [
+        "Prongs, Stag",
+        "Potty, Llama",
+        "Hooves, Donkey",
+        "Moosey, Elk"
+      ]
+    },
+    {
+      "Which of these options was Peter Pettigrews nickname and Animagus form?": [
+        "Wormtail, Rat",
+        "Spaghettiback, Mouse",
+        "Fur-ears, Bat",
+        "Smallsnout, Chinchilla"
+      ]
+    },
+    {
+      "What colour are Harry Potters eyes?": ["Green", "Blue", "Black", "Brown"]
+    },
+    {
+      "What was Remus Lupins nickname and Animagus form?": [
+        "Moony, Werewolf",
+        "Howly, Grey Wolf",
+        "Moony, Grey Wolf",
+        "Howly, Werewolf"
+      ]
+    },
+    {
+      "What is the name of Ron and Hermione's daughter?": [
+        "Rose",
+        "Lily",
+        "Cardi",
+        "Hugo"
+      ]
+    },
+    {
+      "What is the name of Ron and Hermione's son?": [
+        "Hugo",
+        "Ron Junior",
+        "Remus",
+        "Neville"
+      ]
+    },
+    {
+      "Which is the house ghost of Hufflepuff?": [
+        "The Fat Friar",
+        "The Bloody Baron",
+        "The Grey Lady",
+        "Nearly Headless Nick"
+      ]
+    },
+    {
+      "Which champion is the only one to die in the 1994-1995 Triwizard Tournament?": [
+        "Cedric Diggory",
+        "Harry Potter",
+        "Fleur Delacour",
+        "Viktor Krum"
+      ]
+    },
+    {
+      "Who was NOT a member of the 'Slug Club'?": [
+        "Ron",
+        "Harry",
+        "Hermione",
+        "Melinda"
+      ]
+    },
+    {
+      "Which type of magical creature carries Sirius Black away from Hogwarts?": [
+        "Hippogriff",
+        "Thestral",
+        "Unicorn",
+        "Dragon"
+      ]
+    },
+    {
+      "Which is NOT a shop in Diagon Alley?": [
+        "Honeydukes",
+        "Madam Malkin's Robes for All Occasions",
+        "Flourish & Blott's",
+        "Eeylops Owl Emporium"
+      ]
+    },
+    {
+      "Which of the following is NOT a protection for the Philosopher's (Sorcerer's) Stone?": [
+        "Cornish Pixies",
+        "Wizard's Chess ",
+        "Devil's Snare",
+        "Mirror of Erised"
+      ]
+    },
+    {
+      "Which of the following was NOT a Death Eater?": [
+        "Bathilda Bagshot",
+        "Lucius Malfoy",
+        "Draco Malfoy",
+        "Bellatrix Lestrange"
+      ]
+    },
+    {
+      "How many broomsticks are flown in a full game of Quidditch?": [
+        "15",
+        "16",
+        "18",
+        "25"
+      ]
+    },
+    {
+      "Which Polyjuice Potion ingredients must be acquired at the full moon?": [
+        "Fluxweed",
+        "Knotgrass",
+        "A bit of whoever you wish to turn into",
+        "Newt spleen"
+      ]
+    },
+    {
+      "Before using the Firebolt gifted to him in The Prisoner of Azkaban, Harry Potter flew on which of these broomstick models?": [
+        "Nimbus 2000",
+        "Cleansweep Eleven",
+        "Moontrimmer",
+        "Thunderbolt VII"
+      ]
+    },
+    {
+      "The Goblins of Gringotts Wizarding Bank navigate the vaults of their establishment using which of these modes of transportation?": [
+        "Mine cart",
+        "Elevator",
+        "Magic carpet",
+        "Monorail"
+      ]
+    },
+    {
+      "What person from the Wizarding World does Harry discover living just down the road?": [
+        "Mrs. Figg",
+        "Tonks",
+        "Filch",
+        "Susan Bones"
+      ]
+    },
+    {
+      "Floo Powder, as a substance, allows users to travel by which means (as seen in The Chamber of Secrets)?": [
+        "Through fireplaces",
+        "Inside animals",
+        "n the wind",
+        "Through the sewers"
+      ]
+    },
+    {
+      "Who or what attacks Ron in the Ministry of Magic?": [
+        "a brain",
+        "a Time-turner",
+        "Bellatrix",
+        "Dolohov"
+      ]
+    },
+    {
+      "The Knight Bus, used by Harry in The Prisoner of Azkaban, contains how many levels?": [
+        "3",
+        "2",
+        "5",
+        "1"
+      ]
+    },
+    {
+      "Half eagle and half horse, what creature is ridden by Harry and Hermione in The Prisoner of Azkaban after saving its life?": [
+        "Hippogriff",
+        "Centaur",
+        "Dragon",
+        "Griffin"
+      ]
+    }
+  ]
+};
