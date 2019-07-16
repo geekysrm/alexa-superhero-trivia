53fa764b5ab37895ae94568035258d3a3f456eaf
geekysrm <soumyacool2012@gmail.com>
2019-07-17 00:12:09 +0530

    Add questions in EN_CA and EN_AU

--- lambda/custom/questions.js
+++ lambda/custom/questions.js
@@ -715,5 +715,479 @@ module.exports = {
         "A multi-tonal sonic key."
       ]
     }
+  ],
+  QUESTIONS_EN_AU: [
+    {
+      "What is another name for Superman?": [
+        "The Man of Steel",
+        "The Red Redeemer",
+        "The Caped Crusader",
+        "The Masked Avenger"
+      ]
+    },
+    {
+      "What is Superman's only weakness?": [
+        "Kryptonite",
+        "Samsonite",
+        "Plutonite",
+        "Cosmonite"
+      ]
+    },
+    {
+      "What is the name of Batman's secret identity?": [
+        "Bruce Wayne",
+        "Bruce Devon",
+        "The Dark Knight",
+        "Bruce Davis"
+      ]
+    },
+    {
+      "How did Spiderman get his superpowers?": [
+        "He was bitten by a radioactive spider.",
+        "He dies and takes a re birth",
+        "He was caught in a chemical explosion.",
+        "He is fired"
+      ]
+    },
+    {
+      "This superhero's alter ego's name was Lamont Cranston, his show started on radio and eventually branched into comics and other media, and a well-known catchphrase from the show was, 'Who knows what evil lurks in the hearts of men?' Who is he?": [
+        "The Shadow",
+        "Diamond Don",
+        "The Thing",
+        "The Devastator"
+      ]
+    },
+    {
+      "Ant-Man has had a number of incarnations. Which of these was his first?": [
+        "Hank Pym",
+        "Mick Taylor",
+        "Scott Lang",
+        "Eric O'Grady"
+      ]
+    },
+    {
+      "Laura Barton was the wife of which of these Marvel heroes?": [
+        "Hawkeye",
+        "Moonstone",
+        "Paladin",
+        "Captain America"
+      ]
+    },
+    {
+      "A highly trained assassin of Greek descent, she becomes the love interest of Daredevil, who is she?": [
+        "Elektra",
+        "Eon",
+        "Elysius",
+        "Elixir"
+      ]
+    },
+    {
+      "He is able to manipulate energy based on the type of music he is listening to at the time. Who is he better known as?": [
+        "DJ",
+        "Doctor Spectrum",
+        "Domino",
+        "D'Ken"
+      ]
+    },
+    {
+      "Which Marvel Comics character was based on a creation by Mrs Shelley?": [
+        "Frankenstein's Monster",
+        "Falcon",
+        "Firebird",
+        "The Fixer"
+      ]
+    },
+    {
+      "Which Marvel character is sometimes referred to as the Planet Eater?": ["Galactus", "Galaxy Master", "Ganymede", "Gaia"]
+    },
+    {
+      "Portrayed in films by Jon Favreau, Happy Hogan is usually a support character to which of the following?": [
+        "Iron Man",
+        "Black Panther",
+        "Captain America",
+        "Daredevil"
+      ]
+    },
+    {
+      "He's been a regular in the X-Men universe and all he needs is a little bit of water vapour to invoke his powers, who is he?": [
+        "Ice Man",
+        "Impossible Man",
+        "Iso",
+        "Inertia"
+      ]
+    },
+    {
+      "In the Spider Man universe who is the editor-in-chief of the newspaper, The Daily Bugle, who employs Peter Parker as a photographer?": [
+        "J. Jonah Jameson",
+        "Barry O'Hanlon",
+        "Augustus Haynes",
+        "Perry White"
+      ]
+    },
+    {
+      "Wilson Fisk, also known as Kingpin, is the most feared crime lord in the Marvel universe. He made his first appearance in which character's strip?": [
+        "Spider Man",
+        "Hulk",
+        "Iron Man",
+        "Thor"
+      ]
+    },
+    {
+      "Originally published in Marvel's predecessor Atlas Comics, who was proclaimed to be the Queen of the Jungle?": [
+        "Lorna",
+        "Lorelei",
+        "Lucy",
+        "Lani"
+      ]
+    },
+    {
+      "What was the birthname of the powerful Marvel mutant Magneto?": [
+        "Max Eisenhardt",
+        "Malcolm Gregory",
+        "Sandy Stalmaster",
+        "Louis Sadler Jr."
+      ]
+    },
+    {
+      "Blackjack O'Hare appears as an adversary to which anthropomorphic creature, a Guardian of the Galaxy?": [
+        "Rocket Raccoon",
+        "Gunslinger Goanna",
+        "Manslaughter Mouse",
+        "Ballistic Badger"
+      ]
+    },
+    {
+      "Francis Frank Castle, anguished by the murder of his wife and two children turns vigilante and becomes known as which of the following?": [
+        "Punisher",
+        "Peace Monger",
+        "Phantom Rider",
+        "Patriot"
+      ]
+    },
+    {
+      "Pietro Maximoff is known in the Marvel universe as Quicksilver, but which notable adversary of the X-Men proves to be his father?": [
+        "Magneto",
+        "Juggernaut",
+        "Pyro",
+        "Sabretooth"
+      ]
+    },
+    {
+      "Portrayed on the screen by Stellan Skarsgard, Eric Selvig, who becomes involved with the agency known as S.H.I.E.L.D. first appears in which Marvel franchise?": [
+        "Thor",
+        "Hulk",
+        "Ant Man",
+        "Spider Man"
+      ]
+    },
+    {
+      "He made his first appearance in Marvel circles as the Crimson Cowl and became the subject of an Avenger's movie in 2015. Who is he?": [
+        "Ultron",
+        "Ultimaton",
+        "Unus the Untouchable",
+        "Unspoken"
+      ]
+    },
+    {
+      "What is the name of the alien Symbiote that appears in liquid form searching for a host, and its first known host in Marvel's world is the Spider man?": [
+        "Venom",
+        "Vapor",
+        "Valkyrie",
+        "Veil"
+      ]
+    },
+    {
+      "He's an antihero that first appeared on Marvel's pages in 1974, he has powerful regenerative powers, was born James Howlett and has been an X-Man and an Avenger. Who is he?": [
+        "Wolverine",
+        "Wild Thing",
+        "Wolfsbane",
+        "Windeagle"
+      ]
+    },
+    {
+      "In his career Dr. Hank Pym has had a number of different superhero identities; Ant Man, Giant Man, Goliath, the Wasp and which other little stinger?": [
+        "Yellowjacket",
+        "Yellowclaw",
+        "Yamir",
+        "Yondu"
+      ]
+    },
+    {
+      "The first major Marvel/DC crossover was Superman vs. Spider-Man, back in Marvel Treasury Edition no. 28 (1976). Which character did NOT appear in that milestone issue?": [
+        "Lana Lang",
+        "Lex Luthor",
+        "Mary Jane Watson",
+        "Doctor Octopus"
+      ]
+    },
+    {
+      "My civilian name is Barry Allen. I'm a police scientist. My alter ego only has one power, but what DC writers and artists did with that one power was something to behold. Who am I?": [
+        "The Flash",
+        "Green Lantern",
+        "The Atom",
+        "Hawkman"
+      ]
+    },
+    {
+      "J'onn J'onzz was the name of a friendly visitor from a planet in our solar system. What was his alter ego on Earth?": [
+        "The Martian Manhunter",
+        "The Jovian Genie",
+        "The Saturnian Saber Rattler",
+        "The Plutonian Pimpernel"
+      ]
+    },
+    {
+      "What planet and city did Adam Strange zap to when he was hit by the interstellar transporting zeta beam in Mystery In Space?": [
+        "Rann and Ranagar",
+        "Zann and Zanagar",
+        "Mong and Mongopolis",
+        "Krypton and Kandor"
+      ]
+    },
+    {
+      "What did Superman use to open the door of his Fortress of Solitude?": [
+        "An enormous, yellow, arrow-shaped key.",
+        "A hidden remote control.",
+        "His own super-whistle.",
+        "A multi-tonal sonic key."
+      ]
+    }
+  ],
+  QUESTIONS_EN_CA: [
+    {
+      "What is another name for Superman?": [
+        "The Man of Steel",
+        "The Red Redeemer",
+        "The Caped Crusader",
+        "The Masked Avenger"
+      ]
+    },
+    {
+      "What is Superman's only weakness?": [
+        "Kryptonite",
+        "Samsonite",
+        "Plutonite",
+        "Cosmonite"
+      ]
+    },
+    {
+      "What is the name of Batman's secret identity?": [
+        "Bruce Wayne",
+        "Bruce Devon",
+        "The Dark Knight",
+        "Bruce Davis"
+      ]
+    },
+    {
+      "How did Spiderman get his superpowers?": [
+        "He was bitten by a radioactive spider.",
+        "He dies and takes a re birth",
+        "He was caught in a chemical explosion.",
+        "He is fired"
+      ]
+    },
+    {
+      "This superhero's alter ego's name was Lamont Cranston, his show started on radio and eventually branched into comics and other media, and a well-known catchphrase from the show was, 'Who knows what evil lurks in the hearts of men?' Who is he?": [
+        "The Shadow",
+        "Diamond Don",
+        "The Thing",
+        "The Devastator"
+      ]
+    },
+    {
+      "Ant-Man has had a number of incarnations. Which of these was his first?": [
+        "Hank Pym",
+        "Mick Taylor",
+        "Scott Lang",
+        "Eric O'Grady"
+      ]
+    },
+    {
+      "Laura Barton was the wife of which of these Marvel heroes?": [
+        "Hawkeye",
+        "Moonstone",
+        "Paladin",
+        "Captain America"
+      ]
+    },
+    {
+      "A highly trained assassin of Greek descent, she becomes the love interest of Daredevil, who is she?": [
+        "Elektra",
+        "Eon",
+        "Elysius",
+        "Elixir"
+      ]
+    },
+    {
+      "He is able to manipulate energy based on the type of music he is listening to at the time. Who is he better known as?": [
+        "DJ",
+        "Doctor Spectrum",
+        "Domino",
+        "D'Ken"
+      ]
+    },
+    {
+      "Which Marvel Comics character was based on a creation by Mrs Shelley?": [
+        "Frankenstein's Monster",
+        "Falcon",
+        "Firebird",
+        "The Fixer"
+      ]
+    },
+    {
+      "Which Marvel character is sometimes referred to as the Planet Eater?": ["Galactus", "Galaxy Master", "Ganymede", "Gaia"]
+    },
+    {
+      "Portrayed in films by Jon Favreau, Happy Hogan is usually a support character to which of the following?": [
+        "Iron Man",
+        "Black Panther",
+        "Captain America",
+        "Daredevil"
+      ]
+    },
+    {
+      "He's been a regular in the X-Men universe and all he needs is a little bit of water vapour to invoke his powers, who is he?": [
+        "Ice Man",
+        "Impossible Man",
+        "Iso",
+        "Inertia"
+      ]
+    },
+    {
+      "In the Spider Man universe who is the editor-in-chief of the newspaper, The Daily Bugle, who employs Peter Parker as a photographer?": [
+        "J. Jonah Jameson",
+        "Barry O'Hanlon",
+        "Augustus Haynes",
+        "Perry White"
+      ]
+    },
+    {
+      "Wilson Fisk, also known as Kingpin, is the most feared crime lord in the Marvel universe. He made his first appearance in which character's strip?": [
+        "Spider Man",
+        "Hulk",
+        "Iron Man",
+        "Thor"
+      ]
+    },
+    {
+      "Originally published in Marvel's predecessor Atlas Comics, who was proclaimed to be the Queen of the Jungle?": [
+        "Lorna",
+        "Lorelei",
+        "Lucy",
+        "Lani"
+      ]
+    },
+    {
+      "What was the birthname of the powerful Marvel mutant Magneto?": [
+        "Max Eisenhardt",
+        "Malcolm Gregory",
+        "Sandy Stalmaster",
+        "Louis Sadler Jr."
+      ]
+    },
+    {
+      "Blackjack O'Hare appears as an adversary to which anthropomorphic creature, a Guardian of the Galaxy?": [
+        "Rocket Raccoon",
+        "Gunslinger Goanna",
+        "Manslaughter Mouse",
+        "Ballistic Badger"
+      ]
+    },
+    {
+      "Francis Frank Castle, anguished by the murder of his wife and two children turns vigilante and becomes known as which of the following?": [
+        "Punisher",
+        "Peace Monger",
+        "Phantom Rider",
+        "Patriot"
+      ]
+    },
+    {
+      "Pietro Maximoff is known in the Marvel universe as Quicksilver, but which notable adversary of the X-Men proves to be his father?": [
+        "Magneto",
+        "Juggernaut",
+        "Pyro",
+        "Sabretooth"
+      ]
+    },
+    {
+      "Portrayed on the screen by Stellan Skarsgard, Eric Selvig, who becomes involved with the agency known as S.H.I.E.L.D. first appears in which Marvel franchise?": [
+        "Thor",
+        "Hulk",
+        "Ant Man",
+        "Spider Man"
+      ]
+    },
+    {
+      "He made his first appearance in Marvel circles as the Crimson Cowl and became the subject of an Avenger's movie in 2015. Who is he?": [
+        "Ultron",
+        "Ultimaton",
+        "Unus the Untouchable",
+        "Unspoken"
+      ]
+    },
+    {
+      "What is the name of the alien Symbiote that appears in liquid form searching for a host, and its first known host in Marvel's world is the Spider man?": [
+        "Venom",
+        "Vapor",
+        "Valkyrie",
+        "Veil"
+      ]
+    },
+    {
+      "He's an antihero that first appeared on Marvel's pages in 1974, he has powerful regenerative powers, was born James Howlett and has been an X-Man and an Avenger. Who is he?": [
+        "Wolverine",
+        "Wild Thing",
+        "Wolfsbane",
+        "Windeagle"
+      ]
+    },
+    {
+      "In his career Dr. Hank Pym has had a number of different superhero identities; Ant Man, Giant Man, Goliath, the Wasp and which other little stinger?": [
+        "Yellowjacket",
+        "Yellowclaw",
+        "Yamir",
+        "Yondu"
+      ]
+    },
+    {
+      "The first major Marvel/DC crossover was Superman vs. Spider-Man, back in Marvel Treasury Edition no. 28 (1976). Which character did NOT appear in that milestone issue?": [
+        "Lana Lang",
+        "Lex Luthor",
+        "Mary Jane Watson",
+        "Doctor Octopus"
+      ]
+    },
+    {
+      "My civilian name is Barry Allen. I'm a police scientist. My alter ego only has one power, but what DC writers and artists did with that one power was something to behold. Who am I?": [
+        "The Flash",
+        "Green Lantern",
+        "The Atom",
+        "Hawkman"
+      ]
+    },
+    {
+      "J'onn J'onzz was the name of a friendly visitor from a planet in our solar system. What was his alter ego on Earth?": [
+        "The Martian Manhunter",
+        "The Jovian Genie",
+        "The Saturnian Saber Rattler",
+        "The Plutonian Pimpernel"
+      ]
+    },
+    {
+      "What planet and city did Adam Strange zap to when he was hit by the interstellar transporting zeta beam in Mystery In Space?": [
+        "Rann and Ranagar",
+        "Zann and Zanagar",
+        "Mong and Mongopolis",
+        "Krypton and Kandor"
+      ]
+    },
+    {
+      "What did Superman use to open the door of his Fortress of Solitude?": [
+        "An enormous, yellow, arrow-shaped key.",
+        "A hidden remote control.",
+        "His own super-whistle.",
+        "A multi-tonal sonic key."
+      ]
+    }
   ]
 };
